6441d3f8a87101f5584dc4f1e7d61e62308bab85
Sebastian Lundsgaard-Larsen <s_elementos@hotmail.com>
2020-09-29 13:35:58 +0200

    Made the whole project

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MP1_Rest_Client.Models;
+
+namespace MP1_Rest_Client.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly ImageContext _context;
+
+        public ImagesController(ImageContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Images
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Image>>> GetImages(string username = "")
+        {
+            if (string.IsNullOrEmpty(username)) return await _context.Images.ToListAsync();
+
+            return await _context.Images.Where(i => i.UserName == username).ToListAsync();
+        }
+
+        // GET: api/Images/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Image>> GetImage(long id)
+        {
+            var image = await _context.Images.FindAsync(id);
+
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return image;
+        }
+
+
+        //// GET: api/Images/?username={username}
+        //[HttpGet()]
+        //public async Task<ActionResult<IEnumerable<Image>>> GetImagesByUserName(string username)
+        //{
+        //    var images = await _context.Images.Where(i => i.UserName == username).ToListAsync();
+        //    return images;
+        //}
+
+
+        // PUT: api/Images/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutImage(long id, Image image)
+        {
+            if (id != image.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(image).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ImageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Images
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Image>> PostImage(Image image)
+        {
+            _context.Images.Add(image);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetImage", new { id = image.Id }, image);
+        }
+
+        // DELETE: api/Images/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Image>> DeleteImage(long id)
+        {
+            var image = await _context.Images.FindAsync(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            _context.Images.Remove(image);
+            await _context.SaveChangesAsync();
+
+            return image;
+        }
+
+        private bool ImageExists(long id)
+        {
+            return _context.Images.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MP1_Rest_Client.Models
+{
+    public class Image
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string URL { get; set; }
+    }
+}
